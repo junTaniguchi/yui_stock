2d3ed648ea9f7c31d31cfb90517825a2b7387810
juntaniguchi <ultia0602@gmail.com>
2025-07-20 00:41:14 +0900

    f

--- app.py
+++ app.py
@@ -47,6 +47,14 @@ st.markdown(
         color: #222 !important;
         fill: #222 !important;
     }
+    /* サイドバーの背景とテキスト色 */
+    section[data-testid="stSidebar"] {
+        background: #f5fafd !important;
+        color: #222 !important;
+    }
+    section[data-testid="stSidebar"] * {
+        color: #222 !important;
+    }
     </style>
     <div class="bg-illust">
       <svg width="100%" height="100%" viewBox="0 0 400 200" fill="none" xmlns="http://www.w3.org/2000/svg" style="position:absolute;top:0;left:0;opacity:0.13;">
