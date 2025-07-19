8cfe694c27730d45e4a041bf8c759c80aff004aa
juntaniguchi <ultia0602@gmail.com>
2025-07-20 00:28:34 +0900

    [fix] streamlitにfirebaseをsecretとして登録

--- firebase_client.py
+++ firebase_client.py
@@ -1,13 +1,14 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-import os
+import streamlit as st
+import json
 
-# サービスアカウントキーのパス
-SERVICE_ACCOUNT_PATH = os.path.join(os.path.dirname(__file__), "yui-stock-firebase-adminsdk-fbsvc-e789e48bbc.json")
+# SecretsからJSON文字列を取得
+service_account_info = json.loads(st.secrets["FIREBASE_SERVICE_ACCOUNT"])
 
 # Firebase初期化（2重初期化防止）
 if not firebase_admin._apps:
-    cred = credentials.Certificate(SERVICE_ACCOUNT_PATH)
+    cred = credentials.Certificate(service_account_info)
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
